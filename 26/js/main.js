b8b3334caec28066c02a7c084560f72a42a349e9
IgorShapoval <igorshapoval92@gmail.com>
2023-04-23 17:09:44 +0300

    added film info js-ajax

--- 26/js/main.js
+++ 26/js/main.js
@@ -5,10 +5,12 @@ let page = 1;
 const formRef = document.querySelector(".form-inline");
 const filmsListRef = document.querySelector(".films-list");
 const paginationRef = document.querySelector(".pagination");
+const modalRef = document.querySelector(".modal-content");
 
 formRef.addEventListener("submit", onSubmit);
 formRef.addEventListener("input", onSearch);
 paginationRef.addEventListener("click", onButtonClickPage);
+filmsListRef.addEventListener("click", onOpenFilmInfo);
 
 function onSubmit(e) {
   e.preventDefault();
@@ -43,17 +45,35 @@ function getFilms() {
 }
 
 function createMarkupFilmsListItem(films) {
-  const { poster_path, title, release_date } = films;
-  return `<li class="films-list__item">
-            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face${poster_path}" alt="${title}" width="220"/>
-            <div>${title}</div>
-            <div>${release_date}</div>
+  const { poster_path, title, release_date, id } = films;
+
+  return `<li class="films-list__item" aria-id="${id}">
+            <img aria-id="${id}" src="https://www.themoviedb.org/t/p/w220_and_h330_face${poster_path}" alt="${title}" width="220"/>
+            <div aria-id="${id}">${title}</div>
+            <div aria-id="${id}">${release_date}</div>
         </li>`;
+}
+
+function createMarkupFilmsInfo(films) {
+  const { backdrop_path, title, overview, release_date } = films;
+  return `<div class="film-info">
+        <img src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2${backdrop_path}"/>
+        <div class="film-info__wrap">
+          <div>${title}</div>
+          <p>${overview}</p>
+          <div>${release_date}</div>
+        </div>
+  </div>`;
 }
 
 function appendMarkupFilmsList(data) {
   const list = data.map((film) => createMarkupFilmsListItem(film)).join("");
   filmsListRef.insertAdjacentHTML("beforeend", list);
+}
+
+function appendMarkupFilmsInfo(film) {
+  const info = createMarkupFilmsInfo(film);
+  modalRef.insertAdjacentHTML("afterbegin", info);
 }
 
 function clearFilmList() {
@@ -69,4 +89,20 @@ function onButtonClickPage(e) {
   }
 
   getFilms();
+}
+
+function onOpenFilmInfo(e) {
+  const filmId = e.target.getAttribute("aria-id");
+
+  fetch(
+    `https://api.themoviedb.org/3/movie/${filmId}?api_key=${MY_KEY}&language=en-US`
+  )
+    .then((response) => response.json())
+    .then((film) => {
+      const filmInfo = document.querySelector(".film-info");
+      if (filmInfo) {
+        filmInfo.remove();
+      }
+      return appendMarkupFilmsInfo(film);
+    });
 }
